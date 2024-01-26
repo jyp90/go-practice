f51d1479baf55193cd1e59ad0f589a9ec6044170
jyp90 <jypark@estatecloudinc.com>
2024-01-27 00:47:49 +0900

    feat: Fast url checker using Go routines

--- main.go
+++ main.go
@@ -2,21 +2,49 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func main() {
-	c := make(chan string)
-	people := [2]string{"song", "jyp"}
-	for _, person := range people {
-		go isSexy(person, c)
-	}
-	fmt.Println("Waiting for messages")
-	fmt.Println("Received this message: ", <-c)
-	fmt.Println(<-c)
+type RequestResult struct {
+	url    string
+	status string
 }
 
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	c <- person + " is sexy"
+func main() {
+	c := make(chan RequestResult)
+	results := make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+
+}
+
+func hitURL(url string, c chan<- RequestResult) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		c <- RequestResult{url: url, status: "FAILED"}
+	}
+	c <- RequestResult{url: url, status: status}
 }
